08028c86d7563e14323430fbdfc5100bf74e94e5
XtraBunger <aaronyee2005@gmail.com>
2025-02-03 13:33:17 -0800

    File changes

--- AList.cpp
+++ AList.cpp
@@ -1,4 +1,23 @@
+/**
+* File: AListRemoveLeft.cpp
+* Login id: aya102
+* Assignment: A1a
+* Author: Aaron Yee
+* Version: 1.0
+*/
+// I declare that this assignment is my own work and that I have correctly acknowledged the
+// work of others. I acknowledged that I have read and followed the Academic Honesty and
+// Integrity related policies as outlined in the syllabus.
+//
+// The purpose of this assignment was to create the function AList to act as a circular queue
+//
+// _____ Copright Aaron Yee 2025 ____ 
+//
+// ____ 301586491 ______
+//
+
 #include <iostream>
+#include <cassert>
 #include "AList.h" 
 
 int main() {
--- AList.h
+++ AList.h
@@ -1,7 +1,26 @@
+/**
+* File: AListRemoveLeft.cpp
+* Login id: aya102
+* Assignment: A1a
+* Author: Aaron Yee
+* Version: 1.0
+*/
+// I declare that this assignment is my own work and that I have correctly acknowledged the
+// work of others. I acknowledged that I have read and followed the Academic Honesty and
+// Integrity related policies as outlined in the syllabus.
+//
+// The purpose of this assignment was to create the function AList to act as a circular queue
+//
+// _____ Copright Aaron Yee 2025 ____ 
+//
+// ____ 301586491 ______
+//
+
 #ifndef ALIST_H
 #define ALIST_H
 
 #include <iostream>
+#include <cassert>
 #include <iomanip>
 
 template <typename Object>
@@ -9,7 +28,7 @@ template <typename Object>
 class AList {
 
 private:
-    int theSize;
+    int theSize {};
     int left;
     int right;
     int theCapacity;
@@ -48,32 +67,40 @@ public:
     void addLeft(const Object x) {
         // Insert a new object at the left end 
         theSize++;
+        assert(("The size of the array exeeded the capacity",theSize <= theCapacity));
         objects[left] = x;
         left = (left - 1 + theCapacity) % theCapacity;
     }
+        // For this function to work correctly the size must be equal to or less than theCapacity
 
     void addRight(const Object x) {
         // Insert a new object at the right end
         theSize++;
+        assert(("The size of the array exeeded the capacity",theSize <= theCapacity)); 
         objects[right] = x;
         right = (right + 1) % theCapacity;
     }
+        // For this function to work correctly the size must be equal to or less than theCapacity
 
     Object removeRight() {
         // Remove and return the object at the right end
+        assert(("The size of the array cannot be less than 0",theSize > 0));
         theSize--;
         right = (right - 1 + theCapacity) % theCapacity;
         Object x = objects[right];
         return x;
     }
+        // For this function to work correctly the size must be greater than 0
 
     Object removeLeft() {
         // Remove and return the object at the left end
+        assert(("The size of the array cannot be less than 0",theSize > 0));
         theSize--;
         left = (left + 1) % theCapacity;
         Object x = objects[left];
         return x;
     }
+        // For this function to work correctly the size must be greater than 0
 
     void display() const {
         int j = (left + 1) % theCapacity;
